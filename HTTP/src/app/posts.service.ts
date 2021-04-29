2b154bf835ea89d844ed3483c54e34b786ee4f2a
rohithv07 <rohithv63@gmail.com>
2021-04-29 12:53:33 +0530

    Updates: Updated with the basic Post, get, put entrypoint Sets up firebase and conncted with the angular app for the post, get, put operation Error Handled Delete entry point working

---
+++ HTTP/src/app/posts.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import { Post } from "./post.model";
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+
+    error = new Subject<string>();
+
+    constructor(private http: HttpClient) {}
+
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = { title: title, content: content };
+        this.http.post<{ name: string }>(
+            'https://angular-study-http-68235-default-rtdb.firebaseio.com/posts.json', 
+            postData
+            ).subscribe(responseData => {
+              console.log(responseData);
+            }, error => {
+                this.error.next(error.message);
+            });
+    }
+
+    fetchPosts() {
+        return this.http
+    .get< { [key: string]: Post }>('https://angular-study-http-68235-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postsArray: Post[] = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postsArray.push({... responseData[key], id: key})
+        }
+      }
+      return postsArray;
+    }),
+        catchError(errorRes => {
+            // send to analytics error
+            return throwError(errorRes);
+
+        })
+    );
+    }
+
+    deletePosts() {
+        return this.http.delete('https://angular-study-http-68235-default-rtdb.firebaseio.com/posts.json');
+
+    }
+}
