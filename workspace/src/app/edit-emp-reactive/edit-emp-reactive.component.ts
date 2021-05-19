0a4269658173c0928ee9ecd0aa16f7378a73dc7c
rohithv07 <rohithv63@gmail.com>
2021-05-19 12:35:03 +0530

    Updated : Added a new component edit-emp-reactive

---
+++ workspace/src/app/edit-emp-reactive/edit-emp-reactive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-emp-reactive',
+  templateUrl: './edit-emp-reactive.component.html',
+  styleUrls: ['./edit-emp-reactive.component.css']
+})
+export class EditEmpReactiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
