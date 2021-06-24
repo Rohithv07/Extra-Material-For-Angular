9987f5223ecb0d62f623712657187b4b1a04fc39
rohithv07 <rohithv63@gmail.com>
2021-06-24 11:48:42 +0530

    Updates : Practice session on data binding

---
+++ PracticeDataBinding/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+@NgModule({
+  declarations: [AppComponent, DataBindingComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
