1c3afb2dea5c25f9fb2e9c9699579fa5e8e327d5
Rohithv07 <rohithv63@gmail.com>
2021-09-08 16:56:23 +0530

    Learning

---
+++ ChildComponentUsingInput/src/app/address/address.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address',
+  templateUrl: './address.component.html',
+  styleUrls: ['./address.component.css']
+})
+export class AddressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input() address: any;
+
+}
